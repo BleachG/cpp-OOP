846d87d28b1a7eb045ae8435f66e9ce0ca2c146f
BleachG <122334045+BleachG@users.noreply.github.com>
2023-01-25 16:45:56 +0800

    Add files via upload
    一个简单的函数模板

--- 源.cpp
+++ 源.cpp
@@ -2,69 +2,16 @@
 #define max(a,b) (a>b?a:b)
 #include <iostream>
 #include <string.h>
+#include <fstream>
 using namespace std;
-class A
+template<class T>
+void add(T a, T b)
 {
-public:
-	float a,b;
-	A(float m=0, float n=0) { a = m; b = n; };
-	A operator +(A p);
-	A operator -(A p);
-	A operator *(A p);
-	A operator /(A p);
-	friend ostream& operator<<(ostream& os, A am);
-	friend istream& operator>>(istream& is, A& am);
-};
-ostream& operator<<(ostream& os, A am)
-{
-	os << "复数为:";
-	if (am.a != 0)
-	{
-		os << am.a ;
-	}
-	if (am.b != 0)
-	{
-		os << "+" << am.b << "i"<<endl;
-	}
-	return os;
-}
-istream& operator>>(istream& is, A& am)
-{
-	cout << "请输入复数" << endl;
-	is >> am.a;
-	is >> am.b;
-	return is;
-}
-A A::operator+(A p)
-{
-	a = a + p.a;
-	b = b + p.b;
-	return *this;
-}
-A A::operator-(A p)
-{
-	a = a - p.a;
-	b = b - p.b;
-	return *this;
-}
-A A::operator*(A p)
-{
-	float m = a;
-	a = a * p.a - b * p.b;
-	b = m * p.b + b * p.a;
-	return *this;
-}
-A A::operator/(A p)
-{
-	float m = a;
-	a = a * p.a + b * p.b/(p.a*p.a+p.b*p.b);
-	b = b*p.a-m*p.b/(p.a * p.a + p.b * p.b);
-	return *this;
+	cout << a + b << endl;
 }
 int main()
 {
-	A a;
-	cin >> a;
-	cout << a;
+	float a = 1.3, b = 2.61;
+	add(a, b);
 	return 0;
 }
